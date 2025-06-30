65ce22d4e555165a6435b7dd71ceef9e0656ed14
John Sturdy <john.sturdy@planarific.com>
2025-06-30 16:11:18 +0100

    start a demo file

---
+++ src/demo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from dobishem import storage
+
+TABLE_DICT = [
+    {'species': 'felis catus', 'kingdom': 'animal', 'structure': 'quadruped'},
+    {'species': 'amoeba proteus', 'kingdom': 'protist', 'structure': 'unicellular'},
+    {'species': 'octopus vulgaris', 'kingdom': 'animal', 'structure': 'cephalopod'},
+    {'species': 'pringlea antiscorbutica', 'kingdom': 'plant', 'structure': 'herbaceous'},
+]
+
+ORGANISMS_FILE = "/tmp/organisms.csv"
+
+storage.write_csv(ORGANISMS_FILE, TABLE_DICT,
+                  sort_columns=['kingdom'])
+animals = storage.read_csv(ORGANISMS_FILE,
+                           result_type=dict,
+                           key_column='species',
+                           transform_row=lambda r: r if r['kingdom'] == 'animal' else None)
+print(animals)
