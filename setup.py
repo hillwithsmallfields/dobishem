564aba1964cd8bc32d125902e7beb5fe285a37fa
John Sturdy <jcg.sturdy@gmail.com>
2026-02-13 10:02:57 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dobishem",
+    version="0.0.37",
+    description="Python utilities for my other packages",
+    author="John C. G. Sturdy",
+    author_email="jcg.sturdy@gmail.com",
+    packages=find_packages(),
+)
