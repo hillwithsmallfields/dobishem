540048a7484069b0f909c8b6d82d981c36222e28
John C. G. Sturdy <jcg.sturdy@gmail.com>
2026-03-16 14:41:45 +0000

    checkpoint on work on directory handling

--- src/dobishem/storage.py
+++ src/dobishem/storage.py
@@ -58,62 +58,68 @@ class DirectoryAsDictionary:
         self.storage = storage
         self.readable = readable
         self.writable = writable
-        self.__update__listing__()
+        self._update_listing()
 
-    def __update__listing__(self):
+    def _update_listing(self):
         self.filenames = sorted(os.listdir(self.dirname))
 
+    def _file_contents(self, name):
+        return (DirectoryAsDictionary(name)
+                if os.isdir(name)
+                else (self.storage.load(name)
+                      if self.storage
+                      else load(name)))
+
     def __next__(self):
-        print("in DirectoryAsDictionary.__next__")
-        print("iterating over", self.filenames)
         for filename in self.filenames:
             name = os.path.join(self.dirname, filename)
-            data = (self.storage.load(name)
-                    if self.storage
-                    else load(name))
-            print("__next__ yielding", name, "and", len(data), "bytes")
-            yield name, data
+            yield name, self._file_contents(name)
 
     def __iter__(self):
         print("in DirectoryAsDictionary.__iter__")
-        self.__update__listing__()
+        self._update_listing()
         return self.__next__()
 
     def __len__(self):
-        self.__update__listing__()
+        self._update_listing()
         return len(self.filenames)
 
     def items(self):
-        print("in DirectoryAsDictionary.items iterating over", self.filenames)
+        if not self.readable:
+            raise TypeError("This DirectoryAsDictionary object is not readable")
         for filename in self.filenames:
             name = os.path.join(self.dirname, filename)
-            data = (self.storage.load(name)
-                    if self.storage
-                    else load(name))
-            print("items yielding", name, "and", len(data), "bytes")
-            yield name, data
+            yield name, self._file_contents(name)
         # return self.__iter__()
 
     def keys(self):
-        self.__update__listing__()
+        if not self.readable:
+            raise TypeError("This DirectoryAsDictionary object is not readable")
+        self._update_listing()
         return self.filenames
+
+    def values(self):
+        if not self.readable:
+            raise TypeError("This DirectoryAsDictionary object is not readable")
+        self._update_listing()
+        for filename in self.filenames:
+            name = os.path.join(self.dirname, filename)
+            yield self._file_contents(name)
 
     def __contains__(self, key):
         if not self.readable:
             raise TypeError("This DirectoryAsDictionary object is not readable")
-        self.__update__listing__()
+        self._update_listing()
         return key in self.filenames
 
     def __getitem__(self, key):
         if not self.readable:
             raise TypeError("This DirectoryAsDictionary object is not readable")
-        self.__update__listing__()
+        self._update_listing()
         if key not in self.filenames:
             raise FileNotFoundError("No such file or directory: " + key)
         fullname = os.path.join(self.dirname, key)
-        return (self.storage.load(fullname)
-                if self.storage
-                else load(fullname))
+        return self._file_contents(fullname)
 
     def __setitem__(self, key, value):
         if not self.writable:
@@ -125,6 +131,8 @@ class DirectoryAsDictionary:
             save(fullname, value)
 
     def update(self, mapping):
+        if not self.writable:
+            raise TypeError("This DirectoryAsDictionary object is not writable")
         for k, v in mapping.items():
             self[k] = v
 
