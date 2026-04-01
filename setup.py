7717ae0449e4d2c38757d09e891236a8f8fbc1b2
John Sturdy <jcg.sturdy@gmail.com>
2026-04-01 16:26:53 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(
     author="John C. G. Sturdy",
     author_email="jcg.sturdy@gmail.com",
     packages=find_packages(),
+    install_requires=['frozendict', 'collections', 'pickle', 'tempfile', 'yaml']
 )
