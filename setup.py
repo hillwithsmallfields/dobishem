68b914edfa6d89fa1acb7c707d4374cf6a65ef8e
John Sturdy <jcg.sturdy@gmail.com>
2026-06-16 14:44:00 +0100

    package setup fixes

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="dobishem",
-    version="0.0.37",
+    version="0.0.38",
     description="Python utilities for my other packages",
     author="John C. G. Sturdy",
     author_email="jcg.sturdy@gmail.com",
-    packages=find_packages(),
     install_requires=['frozendict', 'collections', 'pickle', 'tempfile', 'yaml']
 )
