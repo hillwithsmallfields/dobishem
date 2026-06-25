a1247a8ad976dad9d8ec1e4a27b85c53239e27e4
John Sturdy <jcg.sturdy@gmail.com>
2026-06-25 21:47:44 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dobishem",
-    version="0.0.39",
+    version="0.0.41",
     description="Python utilities for my other packages",
     author="John C. G. Sturdy",
     author_email="jcg.sturdy@gmail.com",
